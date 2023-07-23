2b0c82b04737dab100a01fb3c6b73a990218b587
ApeIsHere <vladimir.volkov.mg@gmail.com>
2023-07-23 17:21:51 +0300

    Forced_commitment

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,3 +1,5 @@
+import { bind } from "core-js/core/function";
+
 const modals = () => {
     function bindModal(triggerSelector, modalSelector, closeSelector) {
         const trigger = document.querySelectorAll(triggerSelector),
@@ -39,6 +41,7 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
     showModalByTime('.popup', 60000000);
 };
 
